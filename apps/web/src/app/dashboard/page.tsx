e179416725e6a3e0ccbe78f8166eef5813ae64c7
rezkimuhammad60@gmail.com <119469972+rezkimd@users.noreply.github.com>
2025-10-07 21:44:06 +0700

    initial commit

---
+++ apps/web/src/app/dashboard/page.tsx
@@ -0,0 +1,22 @@
+import { redirect } from "next/navigation";
+import Dashboard from "./dashboard";
+import { headers } from "next/headers";
+import { auth } from "@simple-o-web/auth";
+
+export default async function DashboardPage() {
+	const session = await auth.api.getSession({
+		headers: await headers(),
+	});
+
+	if (!session?.user) {
+		redirect("/login");
+	}
+
+	return (
+		<div>
+			<h1>Dashboard</h1>
+			<p>Welcome {session.user.name}</p>
+			<Dashboard session={session} />
+		</div>
+	);
+}
